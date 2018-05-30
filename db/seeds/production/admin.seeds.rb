049b5467bced6a5040c5fea69fd54c179a2a2ab4
Ibrahim Ikhlawi <ikhlawi.ib@gmail.com>
2018-05-30 12:46:46 +0200

    Deleted code from admin.seeds.rb

--- db/seeds/production/admin.seeds.rb
+++ db/seeds/production/admin.seeds.rb
@@ -7,19 +7,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-=begin STDOUT.puts "Enter e-mail for admin:"
-email = STDIN.gets.strip.downcase
-STDOUT.puts "Enter password for admin:"
-pw = STDIN.gets.strip
-STDOUT.puts "Re-enter password for admin:"
-pwc = STDIN.gets.strip
 
-u = User.create(email: email, password: pw, password_confirmation: pwc, name: "Administrator", capabilities:"admin", state:15, account_type: 2)
-if u.save
-  STDOUT.puts "Admin account created!"
-else
-  STDOUT.puts "Something went wrong!"
-end
-
-
-=end
